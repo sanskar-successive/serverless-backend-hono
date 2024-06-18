b59484aa93f72c6c79486b2006f992bd7a5941a2
Sanskar Mishra <sanskar.mishra@sanskarmishra-ss348.successivetech.com>
2024-06-18 23:36:25 +0530

    studied some doc about hono

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { Hono } from 'hono'
-
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('using cloudflare workers with hono!')
-})
+app.get('/', (c) => c.text('Hello Cloudflare Workers!'))
 
 export default app
