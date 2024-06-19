ffe3a6e2e36aa80f769fd5951425d591be6cfae0
Sanskar Mishra <sanskar.mishra@sanskarmishra-ss348.successivetech.com>
2024-06-19 13:21:24 +0530

    prastising using hono

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-```
-npm install
-npm run dev
-```
+Hono is a lightweight, simple, and fast web framework for Cloudflare Workers, Deno, Bun, and other applications. It is a modern web application that is both fast and flexible
 
-```
-npm run deploy
-```
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,55 @@
 import { Hono } from 'hono'
+import { basicAuth } from 'hono/basic-auth'
+// import { serveStatic } from 'hono/serve-static'
+import { cors } from 'hono/cors'
 const app = new Hono()
 
-app.get('/', (c) => c.text('Hello Cloudflare Workers!'))
+
+type BLOG = {
+  id: number,
+  title: string,
+  content: string
+}
+
+const blogs: BLOG[] = [];
+
+
+app.use('/api/*', cors());
+app.use('/api/*', basicAuth({
+  username: 'ankraft',
+  password: '1234'
+}))
+
+// app.use('/static/*', serveStatic({ root: './' }));
+// app.use('/', serveStatic({ path: './index.html' }));
+
+app.get('/api/blogs', (c) => {
+  return c.json(blogs);
+})
+
+app.get('/api/blogs/:id', (c) => {
+  const { id } = c.req.param();
+  const blog = blogs.filter((data) => {
+    if (data.id === parseInt(id)) return 1;
+    return -1;
+  });
+
+  return c.json(blog);
+})
+
+app.post('/api/blogs', async (c) => {
+  const body = await c.req.parseBody() as unknown as BLOG;
+  if (!body) {
+    return c.json({ status: 401, message: 'Request payload is required' });
+  }
+
+  const newBlog = {
+    id: blogs.length + 1,
+    title: body.title,
+    content: body.content
+  }
+  blogs.push(newBlog);
+  return c.json({ data: newBlog });
+})
 
 export default app
